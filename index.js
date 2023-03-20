5a97412e20cee1df14922be68047c3a5a4ae66e2
YeaGram <managerdims@gmail.com>
2023-03-20 20:59:12 +0700

    fiuh

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import expres from 'express'
 const app = expres()
 
 import cors from 'cors'
-app.use(cors())
+app.use(cors({
+    origin: '*',
+}))
 
 app.get('/', (req, res) => {
+    console.log("have request!")
     res.send('Hello World')
 })
+app.listen(process.env.PORT || 3000)
