12f58155dbca1674e8d15880de3b7833dd6a1eb8
YeaGram <managerdims@gmail.com>
2023-03-20 21:05:26 +0700

    add dev

--- index.mjs
+++ index.mjs
@@ -1,13 +1,21 @@
 import expres from 'express'
 const app = expres()
 
+import { PornHub } from 'pornhub.js'
+
 import cors from 'cors'
 app.use(cors({
     origin: '*',
 }))
 
 app.get('/', (req, res) => {
-    console.log("have request!")
-    res.send('Hello World')
+    (async function () {
+        const pornhub = new PornHub()
+    
+        const result = await pornhub.searchVideo('tokyo hot')
+        console.log(result.data[0]) 
+        
+      res.send(result.data[0]);
+     })()
 })
 app.listen(process.env.PORT || 3000)
